855b2fdbab7ab78fd089437b604d6f0e41b8ef20
dominicdaviescodes <codewithdom@gmail.com>
2020-11-18 21:40:41 +0000

    capture value of each clicked pair of cards

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // grab all the cards
-
 const cards = document.querySelectorAll('.memory-card');
+
+// card state (flipped or not yet flipped?)
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
   // console.log('I was clicked');
   // console.log(this); // tells us this is the memory card
-  this.classList.toggle('flip'); // adds or removes .flip
+  this.classList.add('flip'); // adds  .flip
+
+  if (!hasFlippedCard) {
+    // first time clicked a card
+    hasFlippedCard = true;
+    firstCard = this;
+    // console.log(hasFlippedCard, firstCard);
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+    // console.log(firstCard, secondCard);
+
+    // do cards match?
+    // console.log(firstCard.dataset.framework);
+    // console.log(secondCard.dataset.framework);
+  }
 }
 cards.forEach((card) => card.addEventListener('click', flipCard));
